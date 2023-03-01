ebfa862cd0254dc525f936159c1df888071abb7f
Soham Das <Soham.Das@clinisys.com>
2023-03-01 11:20:57 +0530

    updating the JS file

--- site.js
+++ site.js
@@ -8,6 +8,24 @@ function gotoRegister()
     window.location.href = "register.html";
 }
 
+let phoneNo = document.getElementById("phone-no");
+
+if(phoneNo.length < 10 || phoneNo.length > 10)
+{
+    alert("Phone no. should be 10 Digit");
+}
+
+let emailId = document.getElementById("email");
+
+if(emailId.indexOf("@") < 5)
+{
+    alert("Email address not valid");
+}
+else if(!emailId.includes(".com"))
+{
+    alert("Email address not valid");
+}
+
 const urlParams = new URLSearchParams(window.location.search);
 document.getElementById("showName").innerHTML = "Welcome " + urlParams.get('fname') + " " + urlParams.get('lname');
 document.getElementById("showEmail").innerHTML = urlParams.get('email');
